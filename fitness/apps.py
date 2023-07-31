29e2802315579ee67c30a632aff97f7ecfd9b2aa
ABFranco <abfranco4@gmail.com>
2023-07-30 19:12:50 -0700

    Rename app to just "fitness" to make db table names simpler

--- fitness/apps.py
+++ fitness/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FitnessAppConfig(AppConfig):
+class FitnessConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'fitness_app'
+    name = 'fitness'
--- franco_fitness_api/settings.py
+++ franco_fitness_api/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'fitness_app',
+    'fitness',
 ]
 
 MIDDLEWARE = [
